0be3e2c37d7a6e6a7503c4106c45dca654d36895
Soshi <SoshiK@users.noreply.github.com>
2020-12-08 11:37:44 +0900

    GET /api/carsを追加

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
+const db = require("./knex");
 
 //JSONペイロードで受信したリクエストを解析
 app.use(express.json());
+
+//GET /api/cars
+app.get("/api/cars", (req, res) => {
+  db("cars")
+    .join("makers", "cars.maker_id", "=", "makers.id")
+    .then((row) => {
+      console.log(row);
+    })
+    res.send("success!");
+});
 
 app.get("*", (req, res) => {
   res.send("hello");
